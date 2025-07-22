428c32f86df27eaed9352cf5f71d5db27695cb20
Иван <ivanus2004ili@gmail.com>
2025-07-22 18:44:48 +0300

    реализована модель quote, зарегистрирована в админке и добавлены записи

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Quote
 
-# Register your models here.
+@admin.register(Quote)
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ('text_preview', 'author', 'source',
+                    'weight', 'views', 'likes', 'dislikes', 'created_at')
+    search_fields = ('text', 'source')
+    list_filter = ('created_at',)
+
+    def text_preview(self, obj):
+        return obj.text[:50] + '...'
+    text_preview.short_description = 'Цитата'
--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class Quote(models.Model):
+    text = models.TextField()
+    author = models.CharField(max_length=255)
+    source = models.CharField(max_length=255)
+    weight = models.PositiveIntegerField(default=1)
+    views = models.PositiveIntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Цитата'
+        verbose_name_plural = 'Цитаты'
+        constraints =[
+            models.UniqueConstraint(fields=['text', 'source'],
+                                    name = 'unique_quote_per_source')
+        ]
+
+
+    def __str__(self):
+        return f'{self.text[:100]}...({self.source})'
+
+    def clean(self):
+        if Quote.objects.filter(source = self.source).exclude(pk = self.pk).count() >=3:
+            raise ValidationError(f'Нельзя добавить больше 3 цитат для источника "{self.source}"')
